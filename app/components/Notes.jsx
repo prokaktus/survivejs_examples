66e0d480595444c8c58c5747cc44fe0879f12220
prokaktus <proktusfahitasiv@gmail.com>
2015-11-30 03:55:09 +0300

    Initial commit

---
+++ app/components/Notes.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Editable from './Editable.jsx';
+
+export default class Notes extends React.Component {
+    render() {
+        const notes = this.props.items;
+
+        return <ul className="notes">{notes.map(this.renderNote)}</ul>;
+    }
+    renderNote = (note) => {
+        return (
+            <li className="note" key={note.id}>
+                <Editable
+                    value={note.task}
+                    onEdit={this.props.onEdit.bind(null, note.id)}
+                    onDelete={this.props.onDelete.bind(null, note.id)}
+                 />
+            </li>
+        );
+    }
+}
